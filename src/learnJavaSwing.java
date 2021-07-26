712570583a3d037419472ef00b58174c01178744
xyrelljoi <xyrelljoi@gmail.com>
2021-07-26 10:16:06 +0800

    Changes by Arczxc(2)

--- src/learnJavaSwing.java
+++ src/learnJavaSwing.java
@@ -12,5 +12,7 @@ public class learnJavaSwing {
         f.setSize(400,500);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
+
+        System.out.println("mama mo");
     }
 }
