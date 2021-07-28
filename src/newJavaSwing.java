56eed2f0ffbdf28acc510ea374a5348e9417c12b
xyrelljoi <xyrelljoi@gmail.com>
2021-07-28 09:29:31 +0800

    add a button

--- src/newJavaSwing.java
+++ src/newJavaSwing.java
@@ -1,2 +1,9 @@
+import javax.swing.*;
+
 public class newJavaSwing {
+    private JPanel rootPanel;
+    private JLabel lbl1;
+    private JTextField txtfield1;
+    private JButton btn1;
+    private JLabel lbl2;
 }
