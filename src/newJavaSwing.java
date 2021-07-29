75659e5de4bd5bb917e0dac9c6a0dd388e91379f
xyrelljoi <xyrelljoi@gmail.com>
2021-07-30 07:57:30 +0800

    add changes

--- src/newJavaSwing.java
+++ src/newJavaSwing.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class newJavaSwing {
     private JPanel rootPanel;
@@ -6,4 +8,22 @@ public class newJavaSwing {
     private JTextField txtfield1;
     private JButton btn1;
     private JLabel lbl2;
+
+    public newJavaSwing() {
+        btn1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String greeting = "hello! "+txtfield1.getText();
+                lbl2.setText(greeting);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("newJavaSwing");
+        frame.setContentPane(new newJavaSwing().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
